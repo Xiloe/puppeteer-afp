f41b5ebeda5098ba36490a15ca452b0db2ad11b6
Tom Ferriere <xiloe.lepetit@gmail.com>
2024-07-26 19:56:42 +0200

    1.2.0

--- src/index.js
+++ src/index.js
@@ -140,14 +140,7 @@ const protectPage = (page, options = {}) => {
                                     else if (args[0] === 3414) return 24;
                                     else if (args[0] === 36348) return 30;
                                     else if (args[0] === 7936) return 'WebKit';
-                                    else if (args[0] === 37445)
-                                        return window.afpOptions.options.webglData                      ? window.afpOptions.options.webglData['37445']                      : config.random.item([
-                                            'Google Inc.',
-                                            'Google Inc. (Chromium)',
-                                            'Google Inc. (Intel)',
-                                            'Google Inc. (AMD)',
-                                            'Google Inc. (Apple)',
-                                        ]);
+                                    else if (args[0] === 37445) return 'Apple Inc.';
                                     else if (args[0] === 7937) return 'WebKit WebGL';
                                     else if (args[0] === 3379)
                                         return window.afpOptions.options.webglData                      ? window.afpOptions.options.webglData['3378']                      : config.random.number([14, 15]);
